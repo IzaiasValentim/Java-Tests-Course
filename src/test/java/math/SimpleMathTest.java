40cd5b487401c53c1f5cbc4aafe2badbce530b13
luis.aquino <luis.aquino@terceiro.rnp.br>
2024-11-11 10:33:43 -0300

    feat/ Added the first test for sum method success.

--- src/test/java/math/SimpleMathTest.java
+++ src/test/java/math/SimpleMathTest.java
@@ -6,4 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleMathTest {
+
+    @Test
+    @DisplayName("Test 1 + 3 = 4")
+    void test_Sum_Success() {
+        SimpleMath math = new SimpleMath();
+        Double oneSum = math.sum(1D, 3D);
+        assertEquals(4D, oneSum,"The test of sum don't pass :c .");
+        System.out.println("Test of sum is correct.");
+    }
 }
